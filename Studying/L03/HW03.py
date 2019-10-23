426c196419c4fdd09175c97eead2bce051fb0cab
Ihor <galileyforever@gmail.com>
2019-10-23 18:18:50 +0300

    HW03 almost done

--- Studying/L03/HW03.py
+++ Studying/L03/HW03.py
@@ -1,1 +1,35 @@
-print('hello')
+#1. Знайти максимальне число в списку
+
+list = [1, 23, 346, 345, 6, 36, 446, 3732, 4, 2, 2, 2]
+
+# max = 0
+# for i in list:
+#     if i > max:
+#         max = i
+
+# print(max, 'max number')
+
+#2. Знайти суму всіх чисел в списку що діляться на 3
+
+sum = 0
+# for i in list:
+#     if i % 3 == 0:
+#         sum = i + sum
+#         print(i)
+
+# print(sum, 'sum all i')
+
+#3. Вивести квадрати чисел від 10 до 30
+
+# for i in range(10, 30):
+#     print(i ** 2)
+
+#4. вивести середнє значення в списку
+
+# for i in list:
+#     sum = i + sum
+#
+# mid = sum / len(list)
+# print(mid, "mid")
+
+#5.
