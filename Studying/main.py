902d842ccd18a8af775f064850d4b1e47b37c833
Ihor <galileyforever@gmail.com>
2019-11-21 04:23:41 +0200

    HW10 in progg

--- Studying/main.py
+++ Studying/main.py
@@ -1,38 +1,17 @@
-import requests
-import time
+class Mother(object):
+    pass
 
-class Request():
+class Video(object):
+    pass
 
+class HDD(object):
+    pass
+
+
+class BIOS(object):
 
     def __init__(self):
-        self.__sourse = 'https://ramziv.com/ip'
-        self.__t_1 = time.time()
-
-    def get_url(self):
-        return self.__sourse
-
-    def set_url(self, url):
-        self.__sourse = url
-
-    def get_req(self):
-        self.r = requests.get(self.__sourse)
-        return (self.r.content)
-
-        # return (self.r.json())
-
-    url = property(get_url, set_url)
-    req = property(get_req)
-
-
-
-
-r = Request()
-
-r.url = 'https://2ip.ua/ru/'
-
-print(r.req)
-
-
+        self.mother = Mother()
 
 
 
